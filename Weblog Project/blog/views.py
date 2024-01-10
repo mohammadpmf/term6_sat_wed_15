d9eb75919a8ee29d3490b63cb2de6edcea1c14a5
poulstar <poulstar@gmail.ocm>
2024-01-10 16:05:57 +0330

    20 Dey forms.py added

--- Weblog Project/blog/views.py
+++ Weblog Project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import BlogPost
 
 def show_all_posts(request):
@@ -9,5 +9,21 @@ def show_detail(request, pk):
     context = {'post': BlogPost.objects.get(pk=pk)}
     return render(request, 'post_detail.html', context)
 
+from .forms import BlogPostForm
 def new_post(request):
-    return render(request, 'post_new.html')
+    # if request.method=="POST":
+    #     title = request.POST.get("title")
+    #     text = request.POST.get("text")
+    #     author = request.POST.get("author")
+    #     status = request.POST.get("status")
+    #     BlogPost.objects.create(title=title, text=text,
+    #                             author=request.user, status=status)
+    #     return redirect("blog")
+    if request.method=="POST":
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("blog")
+    form = BlogPostForm()
+    return render(request, 'post_new.html', {'form': form})
+
